a871c7825bbd0a431b9e2176e2d5d385ebfeb33f
Huy <minhhuy211@gmail.com>
2022-04-19 14:50:11 +0700

    add Link to Logo

--- src/components/header.js
+++ src/components/header.js
@@ -23,29 +23,31 @@ function Header() {
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                {/* <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
                     <MenuIcon className="block h-6 w-6" aria-hidden="true" />
                   )}
-                </Disclosure.Button>
+                </Disclosure.Button> */}
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
-                  <img
-                    className="block lg:hidden h-8 w-auto"
-                    src={logo}
-                    alt="Workflow"
-                  />
-                  <a href="/">
+                  <Link to="/home">
                     <img
-                      className="hidden lg:block h-12 w-auto"
+                      className="block lg:hidden h-8 w-auto"
                       src={logo}
                       alt="Workflow"
                     />
-                  </a>
+                    <a href="/">
+                      <img
+                        className="hidden lg:block h-12 w-auto"
+                        src={logo}
+                        alt="Workflow"
+                      />
+                    </a>
+                  </Link>
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4 mt-1">
