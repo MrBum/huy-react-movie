e36ea63063d31fb1eace5b9be2fb938d894d3003
Huy <minhhuy211@gmail.com>
2022-06-27 08:55:48 +0700

    fix error

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import {Routes, Route} from "react-router-dom";
+import ContentPage from "../page/Content";
+import PopularMovie from "../page/PopularMovie";
+import MovieDetail from "../components/movie/MovieDetail";
+import Home from "../page/Home";
+import UpcomingMovie from "../page/UpcomingMovie";
+import Search from "../page/Search";
+import CastDetail from "../components/movie/DetailCast";
+
+function RoutesStore() {
+  return (
+    <div>
+      <Routes>
+        <Route exact index element={<Home />} />
+        <Route path="/content" element={<ContentPage />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/popular" element={<PopularMovie />} />
+        <Route path="/upcoming" element={<UpcomingMovie />} />
+        <Route path="/search/:query" element={<Search />} />
+        <Route path="/cast/:id" element={<CastDetail />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default RoutesStore;
